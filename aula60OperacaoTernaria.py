4c26e98972ddd435425f4e4340974e5b206a28a4
Adalto Fernandes <adaltec@gmail.com>
2023-05-30 17:21:35 -0300

    30-05

---
+++ aula60OperacaoTernaria.py
@@ -0,0 +1,17 @@
+'''
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+'''
+
+### IF numa linha só de condição ###
+# condicao = 10 == 10
+# variavel = 'Valor 10' if condicao else 'Outro Valor'  #if numa condição só
+# print(variavel)
+# print('Valor' if True else 'Outro Valor')
+
+# digito = 9
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+
+# print('Valor' if True else 'Outro Valor' if False else 'Fim')
